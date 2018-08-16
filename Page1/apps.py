e54f43cb4c0cbc2e2ec3b67fdd53ce3509375284
Joe Hiton <riyillya@gmail.com>
2018-08-16 10:33:41 +0300

    delete some files

--- Page1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Page1Config(AppConfig):
-    name = 'Page1'
--- Page2/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Page2Config(AppConfig):
-    name = 'Page2'
